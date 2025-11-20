72438713fb8af9e1c4e8c174795cdb19263e23ba
Roger Lo <sobadrush@icloud.com>
2025-11-21 00:11:06 +0800

    「步驟 1」：建立 Player 類別

--- space_invader.py
+++ space_invader.py
@@ -10,7 +10,37 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 
+class Player(pygame.sprite.Sprite):
+    """ 代表玩家的太空船 """
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((50, 25)) # 50x25 的方塊
+        self.image.fill(GREEN) # 綠色
+        self.rect = self.image.get_rect()
+        self.rect.centerx = SCREEN_WIDTH / 2
+        self.rect.bottom = SCREEN_HEIGHT - 10
+        self.speed_x = 0
 
+    def update(self):
+        """ 處理玩家移動 """
+        self.speed_x = 0
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.speed_x = -7
+        if keys[pygame.K_RIGHT]:
+            self.speed_x = 7
+        
+        self.rect.x += self.speed_x
+        
+        # 防止移出視窗
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+    def shoot(self):
+        # 我們等一下再來實作
+        print("SHOOT!")
 
 # --- 遊戲主程式 ---
 pygame.init()
@@ -20,8 +50,8 @@ clock = pygame.time.Clock()
 
 # --- 建立 Sprite Groups ---
 all_sprites = pygame.sprite.Group()
-# player = Player()
-# all_sprites.add(player)
+player = Player()
+all_sprites.add(player)
 
 running = True
 while running:
@@ -32,9 +62,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                # player.shoot()
-                pass
+            if event.key == pygame.K_SPACE: # 按下空格鍵
+                player.shoot()
 
     # --- 遊戲邏輯更新 ---
     all_sprites.update()
